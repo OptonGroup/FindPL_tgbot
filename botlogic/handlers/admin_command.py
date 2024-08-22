e349895ae60725f3557d737da67c06d12d308cc7
Opton <52974908+OptonGroup@users.noreply.github.com>
2024-08-22 23:50:02 +0300

    Update admin_command.py

--- botlogic/handlers/admin_command.py
+++ botlogic/handlers/admin_command.py
@@ -12,6 +12,8 @@ import logging
 from tabulate import tabulate
 
 from botlogic.handlers.base_command import identification_user
+from botlogic.components import keyboard
+
 
 
 async def secret_code_handler(message: Message, state: FSMContext) -> None:
@@ -20,7 +22,8 @@ async def secret_code_handler(message: Message, state: FSMContext) -> None:
     database.user_set_admin(tg_id=user_info['tg_id'], is_admin=1)
         
     await message.answer(
-        'Теперь Вы - Админ. Доступные вам комманды на /admin_commands'
+        'Теперь Вы - Админ. Доступные вам комманды на /admin_commands',
+        reply_markup=keyboard
     )
     
 
@@ -32,7 +35,8 @@ async def admin_commands_handler(message: Message, state: FSMContext) -> None:
         return
         
     await message.answer(
-        'Вы - Админ. Доступные комманды:\n/get_admin_25634 - Получить права админа\n/admin_commands - Получить команды, доступные админам\n/get_user_by_id [user_id] - Получить информацию о пользователе по его [🔑ID]\n/get_user_by_name [username] - Получить информацию о пользователе по его [Имя Пользователя]\n/give_admin [user_id] - Выдать прова админа пользователю по его [🔑ID]\n/remove_admin [user_id] - Забрать права админа пользователя по его [🔑ID]\n/get_users_info - Получить информацию о всех пользователях\n/get_logs - Получить лог файл бота\n/give_sub [user_id] - Выдать подписку пользователю на неделю по его [🔑ID]'
+        'Вы - Админ. Доступные комманды:\n/get_admin_25634 - Получить права админа\n/admin_commands - Получить команды, доступные админам\n/get_user_by_id [user_id] - Получить информацию о пользователе по его [🔑ID]\n/get_user_by_name [username] - Получить информацию о пользователе по его [Имя Пользователя]\n/give_admin [user_id] - Выдать прова админа пользователю по его [🔑ID]\n/remove_admin [user_id] - Забрать права админа пользователя по его [🔑ID]\n/get_users_info - Получить информацию о всех пользователях\n/get_logs - Получить лог файл бота\n/give_sub [user_id] - Выдать подписку пользователю на неделю по его [🔑ID]',
+        reply_markup=keyboard
     )
     
 
@@ -59,7 +63,8 @@ async def get_user_by_id_handler(message: Message, state: FSMContext, command: C
     user_id = command.args
     if not user_id:
         await message.answer(
-            'Необходимо ввести [🔑ID] пользователя. Напимер /get_user_by_id 123456789'
+            'Необходимо ввести [🔑ID] пользователя. Напимер /get_user_by_id 123456789',
+            reply_markup=keyboard
         )
         return
     
@@ -71,12 +76,13 @@ async def get_user_by_id_handler(message: Message, state: FSMContext, command: C
         for row_name in user_info:
             text += f'<b>{row_name}</b> - {user_info[row_name]}\n'
         await message.answer(
-            text
-            
+            text,
+            reply_markup=keyboard
         ) 
     except:
         await message.answer(
-            f'Пользователь с id={user_id} не найден'
+            f'Пользователь с id={user_id} не найден',
+            reply_markup=keyboard
         )
         
 
@@ -90,7 +96,8 @@ async def get_user_by_username_handler(message: Message, state: FSMContext, comm
     username = command.args
     if not username:
         await message.answer(
-            'Необходимо ввести [Имя пользователя]. Напимер /get_user_by_name durov'
+            'Необходимо ввести [Имя пользователя]. Напимер /get_user_by_name durov',
+            reply_markup=keyboard
         )
         return
     
@@ -102,12 +109,14 @@ async def get_user_by_username_handler(message: Message, state: FSMContext, comm
         for row_name in user_info:
             text += f'<b>{row_name}</b> - {user_info[row_name]}\n'
         await message.answer(
-            text
+            text,
+            reply_markup=keyboard
             
         ) 
     except:
         await message.answer(
-            f'Пользователь с username={username} не найден'
+            f'Пользователь с username={username} не найден',
+            reply_markup=keyboard
         )
 
 
@@ -121,18 +130,21 @@ async def give_admin_handler(message: Message, state: FSMContext, command: Comma
     user_id = command.args
     if not user_id:
         await message.answer(
-            'Необходимо ввести [🔑ID] пользователя. Напимер /get_user_by_id 123456789'
+            'Необходимо ввести [🔑ID] пользователя. Напимер /get_user_by_id 123456789',
+            reply_markup=keyboard
         )
         return
     
     try:
         database.user_set_admin(tg_id=user_id, is_admin=1)
         await message.answer(
-            f'Админ-права были выданы пользователю id={user_id}'
+            f'Админ-права были выданы пользователю id={user_id}',
+            reply_markup=keyboard
         )
     except:
         await message.answer(
-            f'Не получилось выдать Админ-права пользователю id={user_id}'
+            f'Не получилось выдать Админ-права пользователю id={user_id}',
+            reply_markup=keyboard
         )
         
 
@@ -146,18 +158,21 @@ async def remove_admin_handler(message: Message, state: FSMContext, command: Com
     user_id = command.args
     if not user_id:
         await message.answer(
-            'Необходимо ввести [🔑ID] пользователя. Напимер /get_user_by_id 123456789'
+            'Необходимо ввести [🔑ID] пользователя. Напимер /get_user_by_id 123456789',
+            reply_markup=keyboard
         )
         return
     
     try:
         database.user_set_admin(tg_id=user_id, is_admin=0)
         await message.answer(
-            f'Забрали Админ-права у пользователя id={user_id}'
+            f'Забрали Админ-права у пользователя id={user_id}',
+            reply_markup=keyboard
         )
     except:
         await message.answer(
-            f'Не получилось забрать Админ-права пользователю id={user_id}'
+            f'Не получилось забрать Админ-права у пользователя id={user_id}',
+            reply_markup=keyboard
         )
         
 
@@ -176,7 +191,7 @@ async def get_users_handler(message: Message, state: FSMContext) -> None:
     with open('files/users_info.txt', 'w', encoding='utf-8') as file:
         file.write(tabulate(data, headers=head, tablefmt="grid"))
     document = FSInputFile('files/users_info.txt')
-    await message.answer_document(document)
+    await message.answer_document(document, reply_markup=keyboard)
     document = ''
     
 
@@ -188,7 +203,7 @@ async def get_logs_handler(message: Message, state: FSMContext) -> None:
         return
     
     document = FSInputFile('info.log')
-    await message.answer_document(document)
+    await message.answer_document(document, reply_markup=keyboard)
     document = ''
     
     
@@ -202,16 +217,19 @@ async def give_sub_handler(message: Message, state: FSMContext, command: Command
     user_id = command.args
     if not user_id:
         await message.answer(
-            'Необходимо ввести [🔑ID] пользователя. Напимер /get_user_by_id 123456789'
+            'Необходимо ввести [🔑ID] пользователя. Напимер /get_user_by_id 123456789',
+            reply_markup=keyboard
         )
         return
     
     try:
         database.user_renew_subscription(tg_id=user_id, amount=0)
         await message.answer(
-            f'Продлили подписку у пользователя id={user_id} на неделю'
+            f'Продлили подписку у пользователя id={user_id} на месяц',
+            reply_markup=keyboard
         )
     except:
         await message.answer(
-            f'Не получилось продлить подписку у пользователя id={user_id}'
+            f'Не получилось продлить подписку у пользователя id={user_id}',
+            reply_markup=keyboard
         )
